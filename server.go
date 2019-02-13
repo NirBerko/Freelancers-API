1fccfad7b186fca14cebc3fe11b650af48e04de9
nirberko <nirberko@gmail.com>
2019-02-13 18:25:16 +0200

    CORSMiddleware

--- server.go
+++ server.go
@@ -20,6 +20,22 @@ func AutoMigrate(db *gorm.DB) {
 	db.AutoMigrate(&models.Project{})
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -46,6 +62,7 @@ func buildRouter(router *gin.Engine, db *gorm.DB) {
 	router.Use(
 		app.Init(),
 		app.Transactional(db),
+		CORSMiddleware(),
 	)
 
 	router.GET("/ping", func(c *gin.Context) {
